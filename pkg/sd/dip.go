0783de067c50ceb4783819054381ea074dfa8cd8
Hoverhuang <hotsym1st@gmail.com>
2022-09-06 08:24:53 +0000

    Add Dir walkfn

--- pkg/sd/dip.go
+++ pkg/sd/dip.go
@@ -8,3 +8,10 @@ type Labels struct {
 	Env string `json:"env"`
 	Job string `json:"job"`
 }
+type FileEncode struct {
+	Encrypt bool  `json:"encrypt"`
+	FileName string `json:"file_name"`
+	FileBodyB64 string `json:"file_body_b64"`
+	LargeFile bool `json:"large_file"`
+	BodySize int64 `json:"body_size"`
+}
